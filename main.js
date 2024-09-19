24956105f627f9f860dde1e6a92c8d8954457490
Yassine Heddate <165506543+Yassinehe10@users.noreply.github.com>
2024-09-19 18:32:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const panoramaImage = new PANOLENS.ImagePanorama("images/image1.jpeg");
+const panoramaImage = new PANOLENS.ImagePanorama("images/13.jpg");
 const imageContainer = document.querySelector(".image-container");
 
 const viewer = new PANOLENS.Viewer({
